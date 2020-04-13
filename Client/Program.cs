b8e494fd5d44629702fd9a395ad2a5591dd6f72f
mertuarez <crasher@inmail.sk>
2020-04-13 22:44:43 +0200

    init

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Api;
+using Microsoft.Rest;
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = new ApiClient(new BasicAuthenticationCredentials());
+
+            bool? x = svc.User.HasUser("asd");
+
+        }
+    }
+}
